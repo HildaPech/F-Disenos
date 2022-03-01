ef68f9d956a40207846907412ba7fe31390792f3
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-01 14:51:12 -0500

    Diseño básico - parte 3

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -7,48 +7,56 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          
-          _crearImagen(), 
-          _crearTitulo(),
-          _crearAcciones(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-          _crearTexto(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[ 
+            _crearImagen(), 
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
       )
     );
   }
   Widget _crearImagen(){
-    return Image(
-    image: NetworkImage('https://cdn.pixabay.com/photo/2019/12/11/21/18/landscape-4689328_960_720.jpg'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage('https://cdn.pixabay.com/photo/2019/12/11/21/18/landscape-4689328_960_720.jpg'),
+        height: 300.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo(){
-    return Container(
-          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-          child: Row(
-            children:  <Widget>[
+    return SafeArea(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Row(
+              children:  <Widget>[
 
-              Expanded(
-                  child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:  <Widget>[
-                    Text('Campo de flores rojas', style: estiloTitulo),
-                    SizedBox(height: 7.0),
-                    Text('Cultivo de flores en Europa', style: estiloSubitulo),        
-                  ],
-                 ),
-              ),
-              Icon(Icons.star, color: Colors.red, size: 30.0),
-              Text('41', style: TextStyle(fontSize: 20.0))
-            ],
-          ),
+                Expanded(
+                    child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:  <Widget>[
+                      Text('Campo de flores rojas', style: estiloTitulo),
+                      SizedBox(height: 7.0),
+                      Text('Cultivo de flores en Europa', style: estiloSubitulo),        
+                    ],
+                   ),
+                ),
+                Icon(Icons.star, color: Colors.red, size: 30.0),
+                Text('41', style: TextStyle(fontSize: 20.0))
+              ],
+            ),
+      ),
     );
   }
 
@@ -74,11 +82,13 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto(){
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Dolor ipsum consectetur laboris nisi irure cillum ex et. Incididunt aute ex proident pariatur. Laborum cillum ea sit cupidatat ex cillum in aute anim sint veniam veniam. Consectetur consectetur incididunt deserunt Lorem adipisicing dolore ex ad enim ut aute id esse. Cupidatat deserunt minim enim dolor occaecat aliquip et et tempor exercitation ullamco veniam mollit. Occaecat qui elit ex aliquip adipisicing ut mollit ea minim. Id occaecat est veniam esse officia exercitation consectetur.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Dolor ipsum consectetur laboris nisi irure cillum ex et. Incididunt aute ex proident pariatur. Laborum cillum ea sit cupidatat ex cillum in aute anim sint veniam veniam. Consectetur consectetur incididunt deserunt Lorem adipisicing dolore ex ad enim ut aute id esse. Cupidatat deserunt minim enim dolor occaecat aliquip et et tempor exercitation ullamco veniam mollit. Occaecat qui elit ex aliquip adipisicing ut mollit ea minim. Id occaecat est veniam esse officia exercitation consectetur.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
