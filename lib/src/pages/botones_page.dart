c7df9e8222bb243cefa00e9142953317562f890a
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-03-02 13:37:14 -0500

    Diseño compuesto - BottomNavigationBar

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -18,7 +18,8 @@ class BotonesPage extends StatelessWidget {
             ),   
           )
         ],
-      )
+      ),
+      bottomNavigationBar: _bottomNavigationBar(context)
     );
   }
 
@@ -80,6 +81,37 @@ class BotonesPage extends StatelessWidget {
             Text('Classify this transaction into a particular category', style: TextStyle(color: Colors.white, fontSize: 18.0))
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _bottomNavigationBar(BuildContext context){
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+        primaryColor: Colors.pinkAccent,
+        textTheme: Theme.of(context).textTheme
+        .copyWith( caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))
+       
+
+      ),
+      
+      child: BottomNavigationBar(
+        fixedColor: Colors.pinkAccent,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            label: ('')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bubble_chart, size: 30.0),
+            label: ('')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle, size: 30.0),
+            label: ('')
+          ),
+        ],
       ),
     );
   }
